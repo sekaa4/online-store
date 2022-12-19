37769c44049a711c7c7c466266f15a560021edb0
Ilya Navumenka <112941578+ilya-navumenka@users.noreply.github.com>
2022-12-19 21:20:26 +0300

    get data (#4)
    * feat: add data.json, getData.ts, dataObject.ts files & fix: tsconfig.json, package.json files
    
    * fix: getData.ts, dataObject.ts, tsconfig.json, package.json files and remove data.json file

---
+++ src/components/getData.ts
@@ -0,0 +1,7 @@
+import { DataObject } from '../interfaces/dataObject';
+
+export default async function getData():Promise<DataObject> {
+  const data: DataObject = await fetch('https://dummyjson.com/products').then((resp) => resp.json());
+  localStorage.setItem('data', JSON.stringify(data.products));
+  return data;
+}
