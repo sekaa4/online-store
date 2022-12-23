b48a416eade26ad3b6a4f487cfb2bff761010e89
Ilya Navumenka <navumenkawork@gmail.com>
2022-12-23 14:01:04 +0300

    refactor: update getData.ts

--- src/components/getData.ts
+++ src/components/getData.ts
@@ -1,7 +1,6 @@
 import { DataObject } from '../interfaces/Data';
 import { LocalStorage } from '../utils/persistentStorage';
 
-
 export default async function getData(): Promise<DataObject> {
   const data: DataObject = await fetch('https://dummyjson.com/products').then((resp) => resp.json());
   const products = data.products;
