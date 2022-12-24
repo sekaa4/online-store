8e6546527826660fba9e733caefb821beb1636cc
Ilya Navumenka <navumenkawork@gmail.com>
2022-12-24 17:51:30 +0300

    feat: add renderCardsColumn function & add cardsColumn styles

--- src/components/render/main.ts
+++ src/components/render/main.ts
@@ -4,6 +4,7 @@ import { ConstantsDom } from '../../models/Dom';
 import { createAside } from '../filters/createAside';
 import createSortSearch from '../sort-products/createSortSearch';
 import { renderCards } from './cards';
+import { renderCardsColumn } from './cardsColumn';
 import { DataProducts } from '../../interfaces/Data';
 
 export function renderMain(data: DataProducts[]): void {
@@ -29,16 +30,24 @@ export function renderMain(data: DataProducts[]): void {
   const sortSearch: CreateElement = createSortSearch();
   itemsContainer.append(sortSearch.elem);
 
-  const cardsWrapper: HTMLElement = createElement(ConstantsDom.DIV, HTMLElement, {
+  // const cardsWrapper: HTMLElement = createElement(ConstantsDom.DIV, HTMLElement, {
+  //   parentElement: itemsContainer,
+  //   classes: [ConstantsDom.CARDS_WRAPPER, ConstantsDom.LAYOUT_5],
+  // });
+
+  const cardsWrapperColumn: HTMLElement = createElement(ConstantsDom.DIV, HTMLElement, {
     parentElement: itemsContainer,
-    classes: [ConstantsDom.CARDS_WRAPPER, ConstantsDom.LAYOUT_5],
+    classes: [ConstantsDom.CARDS_WRAPPER, ConstantsDom.LAYOUT_COLUMN],
   });
 
   //TODO: return instanse of CreateElement mb you'll need a copy in the future.
   const divAside: CreateElement = createAside();
   filterContainer.append(divAside.elem);
-  const divCards = renderCards(data);
-  cardsWrapper.append(...divCards);
+  // const divCards = renderCards(data);
+  // cardsWrapper.append(...divCards);
+
+  const divCards = renderCardsColumn(data);
+  cardsWrapperColumn.append(...divCards);
 
   document.body.append(main.elem);
 }
