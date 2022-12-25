930fb35c7b16b71972fc69a0f4ffc2439952936f
Sergey P <s.pansevi4@gmail.com>
2022-12-25 17:24:42 +0300

    feat: add new listeners in file listeners.ts for work with viewmode

--- src/components/controller/listeners.ts
+++ src/components/controller/listeners.ts
@@ -1,3 +1,26 @@
 import { buildPage } from '../buildPage';
+import getData from '../getData';
+import stateHandler from './stateHandler';
+import history from '../../utils/history';
 
-addEventListener('DOMContentLoaded', buildPage);
+(async function indexPage() {
+  if (localStorage.getItem('data')) {
+    addEventListener('DOMContentLoaded', buildPage);
+    addEventListener('DOMContentLoaded', history);
+    addEventListener('popstate', () => {
+      stateHandler();
+    });
+  } else {
+    addEventListener('DOMContentLoaded', history);
+    const data = await getData();
+
+    if (data.products) {
+      addEventListener('popstate', () => {
+        stateHandler();
+      });
+
+      buildPage();
+      stateHandler();
+    }
+  }
+})();
