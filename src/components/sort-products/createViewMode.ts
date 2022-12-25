914f26ef5b84c56dce8850051230b380cedae7a9
Sergey P <s.pansevi4@gmail.com>
2022-12-25 17:19:23 +0300

    feat: add function createViewMode for viewmode

--- src/components/sort-products/createViewMode.ts
+++ src/components/sort-products/createViewMode.ts
@@ -1,12 +1,15 @@
 import { createElement } from '../elements/generateElement';
+import ControllerElement from '../controller/ControllerElement';
 
 export default function createViewMode(elem: HTMLDivElement): HTMLDivElement {
   const list: HTMLSpanElement = createElement('span', HTMLSpanElement, {
     classes: ['view-mode__list'],
   });
+  const controllerElem = new ControllerElement(elem);
+  controllerElem.start();
 
   const table: HTMLSpanElement = createElement('span', HTMLSpanElement, {
-    classes: ['view-mode__table', 'view-mode_active'],
+    classes: ['view-mode__table', 'view-mode__active'],
   });
 
   elem.append(list, table);
