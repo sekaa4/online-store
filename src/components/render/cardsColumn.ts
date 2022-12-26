bd67d0997607424fa85d9286157e42bdd137029a
Sergey P <s.pansevi4@gmail.com>
2022-12-26 20:41:45 +0300

    fix: fix cards.ts, cardsColumn.ts & main.ts files
    # Conflicts:
    #	src/components/render/cards.ts
    #	src/components/render/main.ts

--- src/components/render/cardsColumn.ts
+++ src/components/render/cardsColumn.ts
@@ -7,43 +7,43 @@ export function createCardColumn(data: DataProducts) {
   const card: Card = new Card(ConstantsDom.ARTICLE, data, {
     classes: [ConstantsDom.CARD_CONTAINER_COLUMN, ConstantsDom.BLOCK_SHADOWED_COLUMN],
   });
-  const div: Card = new Card(ConstantsDom.DIV, data, {
+  const div: HTMLElement = createElement(ConstantsDom.DIV, HTMLElement, {
     parentElement: card.elem,
     classes: [ConstantsDom.CARD_BLOCK_IMAGE_COLUMN],
   });
   createElement(ConstantsDom.IMG, HTMLElement, {
-    parentElement: div.elem,
+    parentElement: div,
     classes: [ConstantsDom.CARD_IMAGE_COLUMN],
     attributes: [
       [ConstantsDom.SRC, data.thumbnail],
       [ConstantsDom.ALT, ConstantsDom.CARD_COLUMN],
     ],
   });
-  const content: Card = new Card(ConstantsDom.DIV, data, {
+  const content: HTMLElement = createElement(ConstantsDom.DIV, HTMLElement, {
     parentElement: card.elem,
     classes: [ConstantsDom.CARD_CONTENT_COLUMN],
   });
   createElement(ConstantsDom.H3, HTMLElement, {
-    parentElement: content.elem,
+    parentElement: content,
     classes: [ConstantsDom.CARD_NAME_COLUMN],
     text: data.title,
   });
   createElement(ConstantsDom.P, HTMLElement, {
-    parentElement: content.elem,
+    parentElement: content,
     classes: [ConstantsDom.CARD_RATING_COLUMN],
     text: `Rating: ${data.rating.toString()}`,
   });
-  const elements: Card = new Card(ConstantsDom.DIV, data, {
-    parentElement: content.elem,
+  const elements: HTMLElement = createElement(ConstantsDom.DIV, HTMLElement, {
+    parentElement: content,
     classes: [ConstantsDom.CARD_ELEMENTS_COLUMN],
   });
   createElement(ConstantsDom.P, HTMLElement, {
-    parentElement: elements.elem,
+    parentElement: elements,
     classes: [ConstantsDom.CARD_PRICE_COLUMN],
     text: `Price: ${data.price.toString()} $`,
   });
   createElement(ConstantsDom.BUTTON, HTMLElement, {
-    parentElement: elements.elem,
+    parentElement: elements,
     classes: [ConstantsDom.BUTTON_CARD_COLUMN, ConstantsDom.BUTTON_CARD_BORDER_COLUMN],
     text: ConstantsDom.BUY,
   });
@@ -53,10 +53,13 @@ export function createCardColumn(data: DataProducts) {
 
 export function renderCardsColumn(data: DataProducts[]): HTMLElement[] {
   elementDomStorage.get(ConstantsDom.CARD_CONTAINER_COLUMN);
-  const arrayData: HTMLElement[] = [];
+  const arrayData: Card[] = [];
+  const arrayDataCard: HTMLElement[] = [];
   data.forEach((cardData) => {
     const card = createCardColumn(cardData);
-    arrayData.push(card.elem);
+    arrayData.push(card);
+    arrayDataCard.push(card.elem);
   });
-  return arrayData;
+  // положить arraydata в localstorage
+  return arrayDataCard;
 }
