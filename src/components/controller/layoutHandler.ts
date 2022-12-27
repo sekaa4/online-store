54207f14f053f1a08e33096773e2b8a503be7400
Sergey P <s.pansevi4@gmail.com>
2022-12-28 01:16:59 +0300

    fix: change behavior ControllerElement class for viewmode

--- src/components/controller/layoutHandler.ts
+++ src/components/controller/layoutHandler.ts
@@ -1,11 +1,4 @@
-import { renderCardsList } from '../render/renderCardsList';
-import { renderCards } from '../render/renderCards';
-import { LocalStorage } from '../../utils/persistentStorage';
-import { DataProducts } from '../../interfaces/Data';
-
 export default function layoutHandler(url: URLSearchParams) {
-  const local: LocalStorage = new LocalStorage();
-  const data: DataProducts[] = local.getItem('data');
   const table: HTMLElement = <HTMLElement>document.querySelector('.view-mode__table');
   const list: HTMLElement = <HTMLElement>document.querySelector('.view-mode__list');
   const cardsWrapper: HTMLElement = <HTMLElement>document.querySelector('.cards__wrapper');
@@ -17,9 +10,6 @@ export default function layoutHandler(url: URLSearchParams) {
     if (cardsWrapper.classList.contains('layout-column')) {
       cardsWrapper.classList.remove('layout-column');
       cardsWrapper.classList.add('layout-5-column');
-      const cardsTable: HTMLElement[] = renderCards(data);
-      cardsWrapper.innerHTML = '';
-      cardsWrapper.append(...cardsTable);
     }
   } else if (url.has('list') && url.get('list') === 'true') {
     table.classList.toggle('view-mode__active', false);
@@ -28,9 +18,13 @@ export default function layoutHandler(url: URLSearchParams) {
     if (cardsWrapper.classList.contains('layout-5-column')) {
       cardsWrapper.classList.remove('layout-5-column');
       cardsWrapper.classList.add('layout-column');
-      const cardsTable: HTMLElement[] = renderCardsList(data);
-      cardsWrapper.innerHTML = '';
-      cardsWrapper.append(...cardsTable);
     }
+  } else if (!url.has('list')) {
+    const table: HTMLElement = <HTMLElement>document.querySelector('.view-mode__table');
+    const list: HTMLElement = <HTMLElement>document.querySelector('.view-mode__list');
+    table.classList.toggle('view-mode__active', true);
+    list.classList.toggle('view-mode__active', false);
+    cardsWrapper.classList.remove('layout-column');
+    cardsWrapper.classList.add('layout-5-column');
   }
 }
