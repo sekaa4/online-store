7d3d079cb55c1ea76ce2ef1bcd44f9fa01cde607
Sergey P <s.pansevi4@gmail.com>
2023-01-09 00:11:23 +0300

    feat: add validate function for the modal window

---
+++ src/components/validate-data/validateEmail.ts
@@ -0,0 +1,31 @@
+import { ConstantsDom } from '../../models/Dom';
+import { createElement } from '../elements/generateElement';
+
+const error: HTMLDivElement = createElement(ConstantsDom.DIV, HTMLDivElement, {
+  classes: [ConstantsDom.MODAL_ERROR],
+  text: 'Error',
+});
+
+const correct: HTMLDivElement = createElement(ConstantsDom.DIV, HTMLDivElement, {
+  classes: [ConstantsDom.MODAL_CORRECT],
+  text: 'Correct',
+});
+
+export function validateEmail(input: HTMLInputElement) {
+  const value = input.value;
+
+  if (
+    /^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:\s@]+)*)|(.+))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      value
+    )
+  ) {
+    error.remove();
+    input.classList.remove('input-error');
+    input.after(correct);
+    console.log('GOOD');
+  } else {
+    input.classList.add('input-error');
+    correct.remove();
+    input.after(error);
+  }
+}
